5b7d3d3efe68beafd48de47cf38e8e443e3bc031
Miroslav Ďurčík / Arki55 <miro.arki55@gmail.com>
2023-01-27 19:56:01 +0100

    arki55/features/T9-github-build-checks-libspectrum fix: +x on bash script, corrected return in case of error

--- .github/scripts/in_config.sh
+++ .github/scripts/in_config.sh
@@ -3,6 +3,6 @@ echo "Looking for line '$1' .."
 lines1=(`cat "./configure.out" | grep -c "$1"`)
 if [[ "$lines1" -ne "1" ]]; then
   echo "Error: Line $1 not found!"
-  return 1
+  exit 1
 fi
 echo "Line '$1' was found."
